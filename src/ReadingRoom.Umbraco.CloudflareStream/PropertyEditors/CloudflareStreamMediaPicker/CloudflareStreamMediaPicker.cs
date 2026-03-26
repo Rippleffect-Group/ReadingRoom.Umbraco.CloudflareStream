dba09c75bdcc3ad29390d667a87e54fee032c581
Matt Burrow <matthew.burrow@fatmedia.co.uk>
2026-03-26 15:27:50 +0000

    Updated the umbraco package json to add back the fields for the editor and picker. Updated the usync files for the test site. Renamed the data types to align with the package.xml.

--- src/ReadingRoom.Umbraco.CloudflareStream/PropertyEditors/CloudflareStreamMediaPicker/CloudflareStreamMediaPicker.cs
+++ src/ReadingRoom.Umbraco.CloudflareStream/PropertyEditors/CloudflareStreamMediaPicker/CloudflareStreamMediaPicker.cs
@@ -12,37 +12,4 @@ public class CloudflareStreamMediaPicker(
     IIOHelper ioHelper)
     : MediaPicker3PropertyEditor(dataValueEditorFactory, ioHelper)
 {
-    protected override IConfigurationEditor CreateConfigurationEditor()
-    {
-        var config = base.CreateConfigurationEditor();
-
-        var fieldsToRemove = new[]
-        {
-            "enableLocalFocalPoint",
-            "crops",
-            "filter"
-        };
-
-        var filterField = config.Fields.FirstOrDefault(x => x.Key == "filter");
-        foreach (var field in fieldsToRemove)
-        {
-            var prop = config.Fields.FirstOrDefault(x => x.Key == field);
-            if (prop != null)
-            {
-                config.Fields.Remove(prop);
-            }
-        }
-
-        if (filterField == null)
-        {
-            return config;
-        }
-
-        filterField.Config = new Dictionary<string, object>
-        {
-            { "itemType", Constants.MediaType.Alias }
-        };
-        config.Fields.Add(filterField);
-        return config;
-    }
 }
